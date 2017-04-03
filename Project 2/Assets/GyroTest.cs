d5c670038e75c1352744fd8c3c3e81ad83dde18e
Ryan Jones <rpj5582@rit.edu>
2017-04-03 14:49:47 -0400

    Trying to test gyro controls

---
+++ Project 2/Assets/GyroTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroTest : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.rotation = GyroToUnity(Input.gyro.attitude);
+        Debug.Log(transform.rotation);
+    }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+}
