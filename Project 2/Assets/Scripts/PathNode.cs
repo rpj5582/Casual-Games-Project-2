0793b5e15acc099668670216ce3df8497e107441
Ryan Gaido (RIT Student) <rwg6766@ad.rit.edu>
2017-04-10 14:52:35 -0400

    branch initilaization rough setup of pathnode tree

---
+++ Project 2/Assets/Scripts/PathNode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode : MonoBehaviour {
+
+    public List<GameObject> subNodes = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < subNodes.Count; i++)
+        {
+            Debug.DrawLine(transform.position, subNodes[i].transform.position, Color.red);
+        }
+	}
+}
