d2309c39924cf645f15d228cea75db817a7cc2ad
Ryan Gaido (RIT Student) <rwg6766@ad.rit.edu>
2017-04-17 14:52:00 -0400

    created customer and table scripts

---
+++ Project 2/Assets/Scripts/CustomerAI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CustomerState
+{
+    IDLE, WALKING, DECIDING, WAITING, EATING, LEAVING
+}
+public class CustomerAI : MonoBehaviour {
+    static float maxSpeed = 1.0f;
+
+    private CustomerState state = CustomerState.IDLE;
+    private List<PathNode> path;
+    private int pathProgress = 0;
+    private PathNode next;
+    private string order = "";
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(state = CustomerState.WALKING)
+        {
+            //seek next
+            //if reached next
+                pathProgress++;
+                next = path[pathProgress];
+        }
+	}
+
+    //tells customer to follow path to its end
+    public void FollowPath(List<PathNode> p)
+    {
+        state = CustomerState.WALKING;
+        path = p;
+
+        pathProgress = 0;
+        next = path[pathProgress];
+    }
+    //tells customer to leave the way they came
+    public void ReversePath()
+    {
+
+    }
+}
---
+++ Project 2/Assets/Scripts/Table_Pathing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table_Pathing : MonoBehaviour {
+    //public const int seatCount = 4;
+    const int seatCount = 4; //currently does not support more than 4-seat square tables
+                             //may come back to that if needed
+
+    public PathNode seat;
+
+    PathNode nearest; //the nearest possible pathnode
+
+    List<PathNode> seats; //pathnodes representing places a customer may sit
+
+    List<PathNode> path; //how to get from origin node to here, should end on [nearest]
+
+
+	// Use this for initialization
+	void Start () {
+
+        seats = new List<PathNode>();	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
