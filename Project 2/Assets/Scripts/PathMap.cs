0f3ec6243ae6f48c30f9e27a7d192b6e69cc2d37
Ryan Gaido (RIT Student) <rwg6766@ad.rit.edu>
2017-04-21 14:54:33 -0400

    progress, table spawns 4 nodes

---
+++ Project 2/Assets/Scripts/PathMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathMap : MonoBehaviour {
+
+	List<PathNode> nodes; //list of all pathnodes
+
+	List<Table_Pathing> tables;//list of all availble tables
+	//index 0 is origin node
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Project 2/Assets/Scripts/Table_Pathing.cs
+++ Project 2/Assets/Scripts/Table_Pathing.cs
@@ -7,7 +7,7 @@ public class Table_Pathing : MonoBehaviour {
     const int seatCount = 4; //currently does not support more than 4-seat square tables
                              //may come back to that if needed
 
-    public PathNode seat;
+    public GameObject seat;
 
     PathNode nearest; //the nearest possible pathnode
 
@@ -15,12 +15,23 @@ public class Table_Pathing : MonoBehaviour {
 
     List<PathNode> path; //how to get from origin node to here, should end on [nearest]
 
+	bool occupied=false; //table is in use by customers
+
+	float tableRadius = 4.0f;
 
 	// Use this for initialization
 	void Start () {
 
         seats = new List<PathNode>();	
 
+		//Vector3 up = transform.up;
+		Vector3 spawn = transform.forward * tableRadius;
+		for (int i = 0; i < seatCount; i++) {
+			GameObject go = Instantiate(seat, transform.position + spawn, transform.rotation) as GameObject;
+			go.transform.parent = transform;
+
+			spawn = Quaternion.AngleAxis(360.0f /seatCount,transform.up)*spawn;
+		}
 	}
 	
 	// Update is called once per frame
