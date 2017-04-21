17b15caa8452d8dca5c85d7ada32c8b1649e243e
MAIN\ltr6702 <ltr6702@g.rit.edu>
2017-04-21 14:36:48 -0400

    Gyro Physics move

---
+++ Project 2/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	public GameObject playerController;
+	public GameObject player;
+	public float torque;
+	private Rigidbody playerBody;
+
+	// Use this for initialization
+	void Start () {
+		playerBody = player.GetComponentInChildren<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerBody.AddTorque(playerController.transform.up * torque * (playerController.transform.forward.x * -1));
+		playerBody.AddTorque(playerController.transform.up * torque * playerController.transform.forward.y * -1);
+		playerBody.AddTorque(playerController.transform.up * torque * playerController.transform.forward.z * -1);
+	}
+}
