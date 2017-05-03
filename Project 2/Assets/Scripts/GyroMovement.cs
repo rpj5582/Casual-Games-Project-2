3b65511a44f06c7f3fe50272bd54e1c25061499c
Ryan Jones <rpj5582@rit.edu>
2017-05-03 14:53:30 -0400

    Removed saving the initial gravity on startup, it was too buggy. Started work on the interaction system

--- Project 2/Assets/Scripts/GyroMovement.cs
+++ Project 2/Assets/Scripts/GyroMovement.cs
@@ -7,31 +7,25 @@ public class GyroMovement : MonoBehaviour
 {
     private Rigidbody rbody;
 
-	private Vector3 initialGravity = Vector3.zero;
     private Vector3 movementVector = Vector3.zero;
 
-	public float movementSpeed = 5;
-	public float sqForceDeadzone = 0.1f;
+	public float movementSpeed = 5f;
+	public float sqForceDeadzone = 0.5f;
 
     // Use this for initialization
     void Start()
     {
         Input.gyro.enabled = true;
-
         rbody = GetComponent<Rigidbody>();
-
-		// Saves the initial gravity vector so that the gyro's initial rotation is the "zero" rotation
-		initialGravity = GyroToUnity(Input.gyro.gravity);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-		movementVector = Vector3.ProjectOnPlane (GyroToUnity(Input.gyro.gravity), initialGravity);
+		movementVector = Vector3.ProjectOnPlane (GyroToUnity(Input.gyro.gravity), Vector3.up);
 		Vector3 force = movementVector * movementSpeed;
 		if (force.sqrMagnitude > sqForceDeadzone)
 		{
-			Debug.Log (force.sqrMagnitude);
 			rbody.AddForce (force);
 		}
     }
