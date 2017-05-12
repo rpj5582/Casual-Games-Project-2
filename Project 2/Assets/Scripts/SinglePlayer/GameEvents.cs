6e8091af8d51a70e94563bfdffb44783e3a2fb23
bluebug <iopzx@gmail.com>
2017-05-12 03:30:03 -0400

    Added static call structures. Hidden unnecessary exposure.

--- Project 2/Assets/Scripts/SinglePlayer/GameEvents.cs
+++ Project 2/Assets/Scripts/SinglePlayer/GameEvents.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameEvents : MonoBehaviour {
-    public static GameEvents Instance;
+    protected static GameEvents Instance;
 
     public delegate void DEL_PLAYER_ASSIGNED(int playerId);
 	public delegate void DEL_MY_PLAYER_MOVED(int playerId, Vector3 position, float timeTaken);
@@ -30,7 +30,7 @@ public class GameEvents : MonoBehaviour {
     }
 
     //When the game starts, game assigns player entity to the player
-    public void mePlayerAssigned(int playerId)
+    void mePlayerAssigned(int playerId)
     {
         for(int i = 0; i < event_player_assigned.Count; i++)
         {
@@ -39,16 +39,23 @@ public class GameEvents : MonoBehaviour {
     }
 
 	//Tells the server to move the player
-    public void myPlayerMoved(int playerId, Vector3 position, float timeTaken)
+    void myPlayerMoved(int playerId, Vector3 position, float timeTaken)
     {
 		for (int i = 0; i < event_my_player_moved.Count; i++)
 		{
 			event_my_player_moved[i](playerId, position, timeTaken);
 		}
     }
-
+    //Game, server, told the player to move this entity to position X within T amount of time.
+    void playerMove(int playerId, Vector3 position, float timeTaken)
+    {
+        for (int i = 0; i < event_other_player_moved.Count; i++)
+        {
+            event_other_player_moved[i](playerId, position, timeTaken);
+        }
+    }
     //Somehow game, server, told client to snap back to position  without any interpolation
-    public void playeMoveInstant(int playerId, Vector3 position)
+    void playeMoveInstant(int playerId, Vector3 position)
     {
         for (int i = 0; i < event_player_moved_instant.Count; i++)
         {
@@ -56,17 +63,10 @@ public class GameEvents : MonoBehaviour {
         }
     }
     
-    //Game, server, told the player to move this entity to position X within T amount of time.
-    public void otherPlayerMove(int playerId, Vector3 position, float timeTaken)
-    {
-		for (int i = 0; i < event_other_player_moved.Count; i++)
-		{
-			event_other_player_moved[i](playerId, position, timeTaken);
-		}
-    }
+  
 
     //Item A is moved onto top of entity B for some reason
-    public void itemMovedTo(int itemId, int entityId)
+    void itemMovedTo(int itemId, int entityId)
     {
 		for (int i = 0; i < event_item_moved.Count; i++)
 		{
@@ -75,7 +75,7 @@ public class GameEvents : MonoBehaviour {
     }
 
     //customer C is moved to an entity; that entity could be a chair or bench or bathroom toilet. Don't care how the movement is handled
-    public void customerMoved(int customerId, int entityId)
+    void customerMoved(int customerId, int entityId)
     {
         for (int i = 0; i < event_customer_moved.Count; i++)
         {
@@ -84,11 +84,68 @@ public class GameEvents : MonoBehaviour {
     }
 
 	//Sets the state of the customer (ie. Satisfied, Unhappy, Angry, etc.)
-    public void customerState(int customerId, int state)
+    void customerState(int customerId, int state)
     {
 		for (int i = 0; i < event_customer_state.Count; i++)
 		{
 			event_customer_state[i](customerId, state);
 		}
+    }
+
+    //Static methods so that you don't need to grasp the "instance"
+
+
+    public static void MY_PLAYER_ASSIGNED(int playerId)
+    {
+        if (Instance == null) return;
+        Instance.mePlayerAssigned(playerId);
+    }
+
+    //Tells the server to move the player
+    public static void MY_PLAYER_MOVED(int playerId, Vector3 position, float timeTaken)
+    {
+
+        if (Instance == null) return;
+        Instance.myPlayerMoved(playerId, position,timeTaken);
+    }
+    
+
+    //Somehow game, server, told client to snap back to position  without any interpolation
+    public static void PLAYER_MOVED_INSTANT(int playerId, Vector3 position)
+    {
+       
+        if (Instance == null) return;
+        Instance.playeMoveInstant(playerId, position);
+
+    }
+
+    //Game, server, told the player to move this entity to position X within T amount of time.
+    public static void PLAYER_MOVED(int playerId, Vector3 position, float timeTaken)
+    {
+
+        if (Instance == null) return;
+        Instance.playerMove(playerId, position, timeTaken);
+        
+    }
+
+    //Item A is moved onto top of entity B for some reason
+    public static void ITEM_MOVE_TO(int itemId, int entityId)
+    {
+        if (Instance == null) return;
+        Instance.itemMovedTo(itemId, entityId);
+    }
+
+    //customer C is moved to an entity; that entity could be a chair or bench or bathroom toilet. Don't care how the movement is handled
+    public static void CUSTOMER_MOVED_TO(int customerId, int entityId)
+    {
+        if (Instance == null) return;
+        Instance.customerMoved(customerId, entityId);
+    }
+
+    //Sets the state of the customer (ie. Satisfied, Unhappy, Angry, etc.)
+    public static void CUSTOMER_STATE(int customerId, int state)
+    {
+        if (Instance == null) return;
+        Instance.customerState(customerId, state);
     }
 }
