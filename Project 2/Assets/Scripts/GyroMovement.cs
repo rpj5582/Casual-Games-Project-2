4461184747218ce429056e7647c3714a0101568b
MAIN\ltr6702 <loganr365@yahoo.com>
2017-05-13 15:16:07 -0400

    I think I possible got re-cailbration.

--- Project 2/Assets/Scripts/GyroMovement.cs
+++ Project 2/Assets/Scripts/GyroMovement.cs
@@ -12,11 +12,14 @@ public class GyroMovement : MonoBehaviour
 	public float movementSpeed = 5f;
 	public float sqForceDeadzone = 0.5f;
 
+	public Vector3 calibrationGravity = new Vector3(0.0f, 0.0f, 0.0f);
     // Use this for initialization
     private void Start()
     {
         Input.gyro.enabled = true;
         rbody = GetComponent<Rigidbody>();
+
+		calibrationGravity = GyroToUnity (Input.gyro.gravity);
     }
 
     private void Update()
@@ -37,7 +40,7 @@ public class GyroMovement : MonoBehaviour
     {
         if(USE_GYRO)
         {
-            Vector3 movementVector = Vector3.ProjectOnPlane(GyroToUnity(Input.gyro.gravity), Vector3.up);
+			Vector3 movementVector = Vector3.ProjectOnPlane(GyroToUnity(Input.gyro.gravity) - calibrationGravity , Vector3.up);
             Vector3 force = movementVector * movementSpeed;
             if (force.sqrMagnitude > sqForceDeadzone)
             {
@@ -51,4 +54,8 @@ public class GyroMovement : MonoBehaviour
     {
 		return new Vector3 (v.x, v.z, v.y);
     }
+
+	public void reCalibrate(){
+		calibrationGravity = GyroToUnity (Input.gyro.gravity);
+	}
 }
