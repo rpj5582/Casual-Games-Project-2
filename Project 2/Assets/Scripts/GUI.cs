13321fcf0918ba29e65a69cb497932164499daa7
Ryan Jones <rpj5582@rit.edu>
2017-05-15 15:46:18 -0400

    Moved customer related scripts to a Customers folder Started making a GUI scripts to handles the options functionality

---
+++ Project 2/Assets/Scripts/GUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GUI : MonoBehaviour
+{
+    public Canvas optionsCanvas;
+    private GameNetworkManager networkManager;
+
+    private void Start()
+    {
+        networkManager = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<GameNetworkManager>();
+    }
+
+    public void ShowOptions()
+    {
+        optionsCanvas.enabled = true;
+    }
+
+    public void HideOptions()
+    {
+        optionsCanvas.enabled = false;
+    }
+
+    public void Recalibrate()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        for (int i = 0; i < players.Length; i++)
+        {
+            Player player = players[i].GetComponent<Player>();
+            if (player.isLocalPlayer)
+            {
+                player.reCalibrate();
+                break;
+            }
+        }
+    }
+
+    public void Disconnect()
+    {
+        networkManager.StopClient();
+    }
+}
