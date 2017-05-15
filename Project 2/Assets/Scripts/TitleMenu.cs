7a4f1ecc1a7acae19863106390c524669c1ca28a
Ryan Jones <rpj5582@rit.edu>
2017-05-15 14:30:14 -0400

    Got the networking stuff working with the title screen and level_1. Now all that's left is to work on gameplay related features, such as spawning customers, taking orders, etc.

---
+++ Project 2/Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TitleMenu : MonoBehaviour
+{
+    public GameNetworkManager networkManager;
+
+    public void StartHost()
+    {
+        networkManager.StartHost(null, 4);
+    }
+
+    public void StartClient()
+    {
+        networkManager.StartClient();
+    }
+}
